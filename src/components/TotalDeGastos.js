8632ed6efd2711c4ae991347986c09a347163591
Alef Porto <falejoaoalef@gmail.com>
2024-11-05 14:27:06 -0300

    Alterando texto do componente TotalDeGastos

--- src/components/TotalDeGastos.js
+++ src/components/TotalDeGastos.js
@@ -2,7 +2,7 @@
 import React from 'react';
 
 function TotalExpenses({ total }) {
-  return <h2>Total: R${total.toFixed(2)}</h2>;
+  return <h2>Total em gastos: R${total.toFixed(2)}</h2>;
 }
 
 export default TotalExpenses;
